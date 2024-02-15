d746b163909ccc756d867c88b98c98f41954bf66
Kobako Annie-neilla <annieneilla.kobako.etu@univ-lille.fr>
2024-02-15 17:23:13 +0100

    ajout  d'un fichier mcu_wc.c

---
+++ mcu/src/mcu_wc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void line_jusqua_bin(){
+    while(getchar()!=EOF);
+}
+int nbr_Car_bus(){
+    for(int i=0;i++;){
+        if(getchar()==EOF)
+        return i;
+    }
+}
+
+
