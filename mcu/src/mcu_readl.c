aa2ffcacabc1d0cc438f291970a255bf066c862d
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-02 21:42:45 +0100

    ajout de la fonction readl

--- mcu/src/mcu_readl.c
+++ mcu/src/mcu_readl.c
@@ -2,20 +2,18 @@
 #include <mcu_fatal.h>
 #include <mcu_macros.h>
 
-int readl(char line[]){
-while(1){
-    fatal (i==MAXLINE, "Erreur: ligne trop longue \n", -1 );
-    char c = getchar();
-
-    fatal(i+1 == )
-    if (c=='\n'){
-        line[i] = '\n';
-        line[i+1] = '\0';
-        return i+1; 
+int readl(char line[]) {
+    int c, i = 0;
+    while ((c = getchar()) != EOF) {
+        line[i++] = c;
+        if(i>MAXLINE){
+            fatal(i >= MAXLINE, "Too long", 2);
+        }
+        if (c == '\n') {
+            line[i] = 0;
+            return i;
+        }
     }
-    if (c ==EOF){
-        line[i] = '\0';
-        return EOF;
-    }
-}
+    line[i] = 0;
+    return EOF;
 }
