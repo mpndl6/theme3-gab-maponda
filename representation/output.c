ef7a97e0a76f453580b0cc1169d8936643665972
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-03 20:50:37 +0100

    Ajout de la boucle

--- representation/output.c
+++ representation/output.c
@@ -9,7 +9,16 @@
 #include <string.h>
 
 int main(void) {
-    struct etudiant_s etudiant;
-    etudiant.numero = 42019008;
-    etudiant.moyenne = 13;
+    struct etudiant_s monEtudiant;
+    monEtudiant.numero = 42019008;
+    monEtudiant.moyenne = 13;
+
+    union bloc_u bloc;
+    bloc.etu = monEtudiant;
+
+    for (size_t i = 0; i < sizeof(struct etudiant_s); i++) {
+        putchar(bloc.data[i]);
+    }
+    return 0;
+
 }
