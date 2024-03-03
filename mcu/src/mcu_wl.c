290f08dd69bac1d14653ea054c6f12864b996d09
gabrielle maponda <gabrielle.mapondalendo.etu@univ-lille.fr>
2024-03-03 17:07:47 +0100

    Ajout du fichier mcu_wc

--- mcu/src/mcu_wl.c
+++ mcu/src/mcu_wl.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-#include "mcu_wl.h"
 
-int main() {
+int count_line(void){
+    int c;
+    int i = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n')
+            i++;
+    }
+    return i;
+}
+int main(void) {
     int n = count_line();
     printf("%i\n", n);
     return 0;
